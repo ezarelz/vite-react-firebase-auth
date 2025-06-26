5b3db34a254be5f143f83c58603a9d4547a73b4f
Ezar <manggala.proflight@gmail.com>
2025-06-27 00:51:18 +0700

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -7,13 +7,13 @@ importScripts(
 );
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDIxgtZZmQSZiALK0hv6xLOnyoW2Yg05Hg',
-  authDomain: 'tes-ezar.firebaseapp.com',
-  projectId: 'tes-ezar',
-  storageBucket: 'tes-ezar.appspot.com',
-  messagingSenderId: '592089826792',
-  appId: '1:592089826792:web:d19a285d2339d76c51905d',
-  measurementId: 'G-FDF2H214H1H',
+  apiKey: 'YOUR_FIREBASE_API',
+  authDomain: 'YOUR_FIREBASE.firebaseapp.com',
+  projectId: 'YOUR_FIREBASE_PROJECT_ID',
+  storageBucket: 'YOUR_FIREBASE.appspot.com',
+  messagingSenderId: 'YOUR_FIREBASE_SenderId',
+  appId: 'YOUR_FIREBASE_appID',
+  measurementId: 'YOUR_FIREBASE_measurementId',
 };
 
 firebase.initializeApp(firebaseConfig);
