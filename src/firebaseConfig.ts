2c97dc0be9324a43ec15c80753d765f9ecbd1b6a
Ezar <manggala.proflight@gmail.com>
2025-06-27 01:42:44 +0700

    firebaseConfig.ts

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -3,14 +3,17 @@ import { initializeApp } from 'firebase/app';
 import { getAnalytics, isSupported } from 'firebase/analytics';
 
 // Konfigurasi Firebase Project
-export const firebaseConfig = {
-  apiKey: 'AIzaSyDIxgtZZm0SZiALK0hv6xLOnyoW2Yg05Hg',
-  authDomain: 'tes-ezar.firebaseapp.com',
-  projectId: 'tes-ezar',
-  storageBucket: 'tes-ezar.appspot.com', // ✅ Diperbaiki
-  messagingSenderId: '592089826792',
-  appId: '1:592089826792:web:d19a285d2339d76c51905d',
-  measurementId: 'G-FDF2H214H1',
+Folder // src/firebaseConfig.ts
+
+// Konfigurasi Firebase Project
+const firebaseConfig = {
+apiKey: 'YOUR_API_KEY',
+authDomain: 'your-app.firebaseapp.com',
+projectId: 'your-app',
+storageBucket: 'your-app.appspot.com',
+messagingSenderId: 'YOUR_MESSAGING_ID',
+appId: 'YOUR_APP_ID',
+measurementId: 'YOUR_MEASUREMENT_ID',
 };
 
 // Inisialisasi Firebase App
